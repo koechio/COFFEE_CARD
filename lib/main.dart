7ce4365c96a743c9289f4ebd3c8993a128dfa431
Allan Koech <allan.koech959@gmail.com>
2026-03-16 13:17:35 -0500

    feat: modularize coffee card into a reusable widget, add spacer between coffee columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,161 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My Coffee Id"),
-        backgroundColor: Colors.brown[700]!,
-        centerTitle: true,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Coffee Id"),
+          backgroundColor: Colors.brown[700]!,
+          centerTitle: true,
         ),
-    body:  const Home(),    
+        body: const Home(),
+      ),
     ),
-  ));
+  );
 }
 
 class Home extends StatelessWidget {
-const Home({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue[700]!,
+      color: Colors.white,
       padding: const EdgeInsets.all(10),
-      
-    child: Row(
-      children: [Expanded(
-        flex: 1,
-      child: Column(
-        children: [Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.green,
-          ),),
-           Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.yellow,
-          ))],
-      
-      ),
-    ),
-      Expanded(
-      flex: 1,
-      child: Column(
-        children: [Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.black,
-            child:const Center( child: Text("Green Box", 
-            style: TextStyle(color: Colors.white, fontSize: 18),
-            ))),
+
+      child: Row(
+        children: [
+          Expanded(
+            // This is the box that represents a vertical split of  the screen
+            flex: 10,
+            child: Column(
+              // the coffee tile tile on top
+              children: [
+                Expanded(
+                  flex: 1,
+
+                  child: Row(
+                    children: [
+                      Expanded(
+                  flex: 1,
+                  child: CoffeeTile(
+                  name: "Espresso Coffee",
+                  imagepath: 'assets/images/Mocha.jpg', 
+                  price: "30.00")
+                ),
+                    ],
+                  ),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: CoffeeTile(
+                  name: "Espresso Coffee",
+                  imagepath: 'assets/images/Mocha.jpg', 
+                  price: "30.00")
+                ),
+              ],
+            ),
           ),
-           Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.pink,
-          ))],
-      
+          const Spacer(flex: 1,),
+          Expanded(
+            // Second Half of the screen
+            flex: 10,
+            child: Column(
+              // The Top Column
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: CoffeeTile(
+                  name: "Espresso Coffee",
+                  imagepath: 'assets/images/Mocha.jpg', 
+                  price: "30.00")
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: CoffeeTile(
+                  name: "Espresso Coffee",
+                  imagepath: 'assets/images/Mocha.jpg', 
+                  price: "30.00")
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-    ),  ]
-    
-    )
-    
-      
-      
-      
-      
-      
-      
-      );
+    );
   }
 }
 
+class CoffeeTile extends StatelessWidget {
+  final String name;
+  final String imagepath;
+  final String price;
+  const CoffeeTile(
+  {super.key, 
+  required this.name,
+  required this.imagepath,
+  required this.price,} );
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Image.asset(
+                                // the image in the coffee Tile
+                                imagepath,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+
+                            Expanded(
+                              // Lower part of cofee
+                              flex: 1,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children:[
+                                        Text(// This is the text, The name of the coffee
+                                        name,
+                                        style: TextStyle(
+                                                          fontSize: 20,
+                                                          fontWeight: FontWeight.bold,),
+                                                          ),
+ 
+                                    const SizedBox(height: 4,),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                      Icon(Icons.star, color: Colors.brown, size: 12,),
+                                      Icon(Icons.star, color: Colors.brown, size: 12,),
+                                      Icon(Icons.star, color: Colors.brown, size: 12,),
+                                      Icon(Icons.star, color: Colors.brown, size: 12,),
+                                      Icon(Icons.star, color: Colors.brown, size: 12,)
+
+                                      ],
+                                    ),
+                                      //This is the price block
+                                      Text(
+                                        "\$ $price",
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      ],
+                                  ),
+                                  ),
+                                ],
+                            
+                              ),),],);
+                          
+  
+  }
+}
